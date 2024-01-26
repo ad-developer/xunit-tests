5d149e477af8eb07ed2e7c056fd0350cb742e98a
A.D <ad@Alexandrs-MBP.lan>
2024-01-25 20:51:41 -0700

    increment

---
+++ TestRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+class Programm
+{
+
+    static int Main(string[] args)
+    {
+        
+        return 0;
+    }
+}
