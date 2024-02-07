142747486b56f44badb68cd51013ccb46d852482
Alexandr Dolenko <ad@Alexandrs-MBP.lan>
2024-02-07 15:15:10 -0700

    increment

--- UnitTests/Tests/AdvancedTest/TestMockSubsctitute.cs
+++ UnitTests/Tests/AdvancedTest/TestMockSubsctitute.cs
@@ -1,27 +1,25 @@
-﻿using Applications.CustormerApp;
-using Microsoft.EntityFrameworkCore;
-using NSubstitute;
+﻿using System.Xml.Linq;
+using Applications.CustormerApp;
+using Castle.Core.Resource;
 using UnitTests.Fixtures;
 
 namespace UnitTests.Tests.AdvancedTest
 {
     public class TestMockSubsctitute
 	{
-        private readonly ICustomerService _sut;
-        private readonly IApplicationDBContext _context;
-
         public TestMockSubsctitute()
 		{
-            var customers = new List<Customer>(); 
-            _context = ApplicationDbContextFixture.Create(customers);
-
-            _sut = new CustomerService(_context);
+           
         }
 
         [Fact]
         [Trait("Application", "Customer Service")]
         public void AddTest()
         {
+            var customers = new List<Customer>();
+            var context = ApplicationDbContextFixture.Create(customers);
+
+            var sut = new CustomerService(context);
 
             var customerFixture = new Customer
             {
@@ -32,25 +30,79 @@ namespace UnitTests.Tests.AdvancedTest
                 Phone = "333.333.4444"
             };
 
-            _sut.Add(customerFixture);
-            var customer = _context.Customers.First();
+            sut.Add(customerFixture);
+            var customer = context.Customers.First();
            
             Assert.True(customer.Id > 0, $"Customer Id is {customer.Id} and it's more than 0");
             Assert.True(customer.FirstName == "John", "Customer first name is John");
         }
 
-        [Fact]
+        [Theory]
+        [InlineData(3, "John")]
+        [InlineData(5, "Michael")]
         [Trait("Application", "Customer Service")]
-        public void GetCustomerByIdTest()
+        public void GetCustomerByIdTest(int id, string name)
         {
+            var customers = new List<Customer>();
+            var customerFixtureOne = new Customer
+            {
+                Id = 3,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "John.Smith@email.com",
+                Phone = "333.333.4444"
+            };
 
+            var customerFixtureTwo = new Customer
+            {
+                Id = 5,
+                FirstName = "Michael",
+                LastName = "Jones",
+                Email = "Michael.Jones@email.com",
+                Phone = "333.333.5555"
+            };
+            customers.Add(customerFixtureOne);
+            customers.Add(customerFixtureTwo);
+
+            var context = ApplicationDbContextFixture.Create(customers);
+
+            var sut = new CustomerService(context);
+
+            var customer = sut.GetCustomerById(id);
+            Assert.True(customer.FirstName == name, $"Customer first name is {name}");
         }
 
         [Fact]
         [Trait("Application", "Customer Service")]
         public void GetAllCustomersTest()
         {
+            var customers = new List<Customer>();
+            var customerFixtureOne = new Customer
+            {
+                Id = 3,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "John.Smith@email.com",
+                Phone = "333.333.4444"
+            };
 
+            var customerFixtureTwo = new Customer
+            {
+                Id = 5,
+                FirstName = "Michael",
+                LastName = "Jones",
+                Email = "Michael.Jones@email.com",
+                Phone = "333.333.5555"
+            };
+            customers.Add(customerFixtureOne);
+            customers.Add(customerFixtureTwo);
+
+            var context = ApplicationDbContextFixture.Create(customers);
+
+            var sut = new CustomerService(context);
+
+            var customer = sut.GetAllCustomers();
+            Assert.True(customer.Count == 2, "Total number of customers is 2");
         }
     }
 }
